bab5abce48c05f97d99a13dfa766e815e8750a23
George Nicoll <george.nicoll@gmail.com>
2024-12-07 06:15:56 +0000

    Part1 plus tiny change for part 2

--- day7/main.zig
+++ day7/main.zig
@@ -9,13 +9,25 @@ const Context = struct {
     delimiters: std.AutoHashMap(u8, bool),
 };
 
-const Line = struct {};
+const Num = u64;
+
+const Line = struct {
+    result: Num,
+    numbers: []const Num,
+
+    fn print(self: Line, writer: anytype) !void {
+        try writer.print("{d}: ", .{self.result});
+        for (self.numbers) |number| {
+            try writer.print("{d} ", .{number});
+        }
+    }
+};
 
 pub fn main() !void {
     const day = "day7";
-    const file_name = day ++ "/test_file.txt";
+    //const file_name = day ++ "/test_file.txt";
     //const file_name = day ++ "/test_cases.txt";
-    //const file_name = day ++ "/input.txt";
+    const file_name = day ++ "/input.txt";
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -25,6 +37,7 @@ pub fn main() !void {
 
     var delimiters = std.AutoHashMap(u8, bool).init(arena_allocator.allocator());
     try delimiters.put(' ', true);
+    try delimiters.put(':', true);
 
     var context = Context{
         .delimiters = delimiters,
@@ -37,37 +50,127 @@ pub fn main() !void {
     );
     defer parsed_lines.deinit();
 
-    // const stdout = std.io.getStdOut();
-    // for (context.grid.items) |row| {
-    //     try stdout.writeAll(row.items);
-    //     try stdout.writeAll("\n");
-    // }
-    // try stdout.writer().print("width: {d}, height: {d}\n", .{ context.width, context.height });
+    const stdout = std.io.getStdOut();
+    for (parsed_lines.items) |line| {
+        try line.print(stdout.writer());
+        try stdout.writeAll("\n");
+    }
 
-    try calculate(arena_allocator.allocator(), context);
-    try calculate_2(arena_allocator.allocator(), context);
+    try calculate(arena_allocator.allocator(), parsed_lines);
+    try calculate_2(arena_allocator.allocator(), parsed_lines);
 }
 
 fn parse_line(allocator: std.mem.Allocator, context: *Context, line: []const u8) !Line {
-    _ = allocator;
-    _ = context;
-    _ = line;
-    return .{};
+    var parser = process.LineParser().init(allocator, context.delimiters, line);
+    defer parser.deinit();
+
+    const result = try parser.read_int(Num, 10);
+    var numbers = std.ArrayList(Num).init(allocator);
+    defer numbers.deinit();
+
+    var number = parser.read_int(Num, 10) catch null;
+    while (number != null) {
+        try numbers.append(number.?);
+        number = parser.read_int(Num, 10) catch null;
+    }
+
+    return .{
+        .result = result,
+        .numbers = try numbers.toOwnedSlice(),
+    };
 }
 
-fn calculate(allocator: std.mem.Allocator, context: Context) !void {
-    _ = allocator;
-    _ = context;
+const Op = enum {
+    Add,
+    Multiply,
+
+    fn calculate(self: Op, a: Num, b: Num) Num {
+        return switch (self) {
+            Op.Add => |_| a + b,
+            Op.Multiply => |_| a * b,
+        };
+    }
+};
+
+const Level = struct {
+    op: Op,
+    level: usize,
+    result_so_far: Num,
+};
+
+fn is_possible(line: Line, stack: *std.ArrayList(Level), allowed_ops: []const Op) !bool {
+    stack.clearRetainingCapacity();
+
+    //first ops. level correspond to left number's index
+    for (allowed_ops) |op| {
+        try stack.append(Level{
+            .op = op,
+            .level = 0,
+            .result_so_far = line.numbers[0],
+        });
+    }
+
+    try stack.append(Level{
+        .op = Op.Multiply,
+        .level = 0,
+        .result_so_far = line.numbers[0],
+    });
+
+    while (stack.items.len > 0) {
+        const level = stack.pop();
+
+        const next_level = level.level + 1;
+        const next_value = line.numbers[next_level];
+        const next_result_so_far = level.op.calculate(level.result_so_far, next_value);
+
+        //did we reach the end??? check the value - we don't add any more
+        if (next_level >= line.numbers.len - 1) {
+            if (next_result_so_far == line.result) {
+                return true;
+            }
+            continue;
+        }
+
+        //If this is greater or equal to expected, stop looking on this branch now
+        if (next_result_so_far > line.result) {
+            continue;
+        }
+
+        //Otherwise, search the next 2 operations
+        try stack.append(Level{
+            .op = Op.Add,
+            .level = next_level,
+            .result_so_far = next_result_so_far,
+        });
+        try stack.append(Level{
+            .op = Op.Multiply,
+            .level = next_level,
+            .result_so_far = next_result_so_far,
+        });
+    }
+
+    return false; //nothing worked
+}
+
+fn calculate(allocator: std.mem.Allocator, lines: std.ArrayList(Line)) !void {
+    //depth first search
+    var stack = try std.ArrayList(Level).initCapacity(allocator, 2000);
+    defer stack.deinit();
 
     var sum: usize = 0;
-    sum += 0;
+
+    for (lines.items) |line| {
+        if (try is_possible(line, &stack)) {
+            sum += line.result;
+        }
+    }
 
     try std.io.getStdOut().writer().print("Part 1 Sum {d}\n", .{sum});
 }
 
-fn calculate_2(allocator: std.mem.Allocator, context: Context) !void {
+fn calculate_2(allocator: std.mem.Allocator, lines: std.ArrayList(Line)) !void {
     _ = allocator;
-    _ = context;
+    _ = lines;
 
     var sum: usize = 0;
     sum += 0;
--- shared/regex.zig
+++ shared/regex.zig
@@ -9,50 +9,68 @@ const c = @cImport({
 const Matches = struct {
     const Self = @This();
 
-    matches: *std.ArrayList(*std.ArrayList(u8)),
+    allocator: std.mem.Allocator,
+    matches: std.ArrayList(*std.ArrayList(u8)),
 
     fn init(allocator: std.mem.Allocator) !Self {
-        const matches = try allocator.create(std.ArrayList(*std.ArrayList(u8)));
-        matches.* = std.ArrayList(*std.ArrayList(u8)).init(allocator);
+        const matches = std.ArrayList(*std.ArrayList(u8)).init(allocator);
         return .{
+            .allocator = allocator,
             .matches = matches,
         };
     }
 
-    pub fn deinit(self: *Self) void {
+    pub fn deinit(self: Self) void {
         for (self.matches.items) |match| {
-            match.deinit();
+            match.*.deinit();
         }
         self.matches.deinit();
     }
 
-    fn appendMatch(self: *Self, allocator: std.mem.Allocator, match: []const u8) !void {
-        const map: *std.ArrayList(u8) = try allocator.create(std.ArrayList(u8));
-        map.* = std.ArrayList(u8).init(allocator);
+    fn appendMatch(self: *Self, match: []const u8) !void {
+        const map: *std.ArrayList(u8) = try self.allocator.create(std.ArrayList(u8));
+        map.* = std.ArrayList(u8).init(self.allocator);
         try map.*.appendSlice(match);
         try self.matches.append(map);
     }
 };
 
-const Regex = struct {
-    inner: *c.regex_t,
+const RegexTStart = struct {
+    buffer: c_uint, //pointer
+    allocated: c_ulong,
+    used: c_ulong,
+    syntax: c_ulong,
+    fastmap: usize, //pointer
+    translate: usize, //pointer
+    re_nsub: usize,
+};
 
-    fn init(pattern: [:0]const u8) !Regex {
+const Regex = struct {
+    const Self = @This();
+
+    inner: *c.regex_t,
+    regext_start: *RegexTStart,
+
+    fn init(pattern: [:0]const u8) !Self {
         const inner = c.alloc_regex_t().?;
         if (0 != c.regcomp(inner, pattern, c.REG_NEWLINE | c.REG_EXTENDED)) {
             return error.compile;
         }
-
         return .{
             .inner = inner,
+            .regext_start = @ptrCast(@alignCast(inner)),
         };
     }
 
-    fn deinit(self: Regex) void {
+    fn deinit(self: Self) void {
         c.free_regex_t(self.inner);
     }
 
-    fn matches(self: Regex, input: [:0]const u8) bool {
+    pub fn numSubExpressions(self: Self) usize {
+        return self.regext_start.re_nsub;
+    }
+
+    pub fn matches(self: Self, input: [:0]const u8) bool {
         const match_size = 1;
         var pmatch: [match_size]c.regmatch_t = undefined;
         return 0 == c.regexec(self.inner, input, match_size, &pmatch, 0);
@@ -60,7 +78,7 @@ const Regex = struct {
 
     /// Execute the regex against the input string.
     /// Returns an array list containing array lists for each match - all should be cleaned up
-    fn exec(self: Regex, allocator: std.mem.Allocator, input: [:0]const u8) !Matches {
+    pub fn exec(self: Self, allocator: std.mem.Allocator, input: [:0]const u8) !Matches {
         const match_size = 1;
         var pmatch: [match_size]c.regmatch_t = undefined;
 
@@ -72,10 +90,17 @@ const Regex = struct {
                 break;
             }
 
-            const slice = string[@as(usize, @intCast(pmatch[0].rm_so))..@as(usize, @intCast(pmatch[0].rm_eo))];
-            try re_matches.appendMatch(allocator, slice);
+            const start_match = @as(usize, @intCast(pmatch[0].rm_so));
+            const end_match = @as(usize, @intCast(pmatch[0].rm_eo));
 
-            string = string[@intCast(pmatch[0].rm_eo)..];
+            if (start_match == end_match) {
+                break;
+            }
+
+            const slice = string[start_match..end_match];
+            try re_matches.appendMatch(slice);
+
+            string = string[end_match..];
         }
 
         return re_matches;
@@ -90,6 +115,7 @@ test "matches simple" {
     const regex1 = try Regex.init("nice");
     defer regex1.deinit();
 
+    try expect(regex1.numSubExpressions() == 0);
     try expect(regex1.matches("this should match nice!"));
 }
 
@@ -97,16 +123,37 @@ test "exec simple" {
     const regex1 = try Regex.init("nice");
     defer regex1.deinit();
 
+    try expect(regex1.numSubExpressions() == 0);
+
     const result = try regex1.exec(testing_alloc, "this should nice match nice");
+    defer result.deinit();
     try expect(result.matches.items.len == 2);
     try expect(eql(u8, result.matches.items[0].items, "nice"));
     try expect(eql(u8, result.matches.items[1].items, "nice"));
 }
 
+test "exec match :digit:" {
+    const regex1 = try Regex.init("[[:digit:]]+");
+    defer regex1.deinit();
+
+    try expect(regex1.numSubExpressions() == 0);
+
+    const result = try regex1.exec(testing_alloc, "bob 123 rita 456 sue: 85");
+    defer result.deinit();
+    try expect(result.matches.items.len == 3);
+    try expect(eql(u8, result.matches.items[0].items, "123"));
+    try expect(eql(u8, result.matches.items[1].items, "456"));
+    try expect(eql(u8, result.matches.items[2].items, "85"));
+}
+
 test "matches" {
-    const regex1 = try Regex.init(".*mul\\([:digit:]*,[:digit:]\\).*");
+    const regex1 = try Regex.init("mul([:digit:]*,[:digit:]*)");
     defer regex1.deinit();
 
     try expect(regex1.matches("do()something((mul(123,456)))))()"));
     try expect(!regex1.matches("do()something((mu(123,456)))))()"));
+}
+
+test "sub_expressions" {
+    try expect(false);
 }
